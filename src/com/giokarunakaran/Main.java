1a8703fa5f02b50f7dfa898959b1dd53657ec435
Gio K <admin@users-Air.broadband>
2020-05-14 10:20:43 +0100

    Code for running application

--- src/com/giokarunakaran/Main.java
+++ src/com/giokarunakaran/Main.java
@@ -4,25 +4,25 @@ public class Main {
 
     public static void main(String[] args) {
 
-
-        Bank bank = new Bank("LLoyds Bank");
-
-        bank.addBranch("Enfield");
-
-        bank.addCustomer("Enfield", "Gio", 200.00);
-        bank.addCustomer("Enfield", "Sanj", 50.00);
-        bank.addCustomer("Enfield", "Jo", 100.00);
-
-        bank.addBranch("Southampton");
-        bank.addCustomer("Southampton", "Megs", 50.00);
-
-        bank.addCustomerTransaction("Enfield", "Gio", 100.00);
-        bank.addCustomerTransaction("Enfield", "Gio", 50.00);
-        bank.addCustomerTransaction("Southampton", "Megs", 20.00);
-
-        bank.listCustomers("Enfield", true);
-        bank.listCustomers("Southampton", true);
-        bank.addCustomer("Enfield", "Megs", 30.00);
-        bank.addCustomer("Southampton", "Megs", 20.00);
+//        E.g.
+//        Bank bank = new Bank("LLoyds Bank");
+//
+//        bank.addBranch("Enfield");
+//
+//        bank.addCustomer("Enfield", "Gio", 200.00);
+//        bank.addCustomer("Enfield", "Sanj", 50.00);
+//        bank.addCustomer("Enfield", "Jo", 100.00);
+//
+//        bank.addBranch("Southampton");
+//        bank.addCustomer("Southampton", "Megs", 50.00);
+//
+//        bank.addCustomerTransaction("Enfield", "Gio", 100.00);
+//        bank.addCustomerTransaction("Enfield", "Gio", 50.00);
+//        bank.addCustomerTransaction("Southampton", "Megs", 20.00);
+//
+//        bank.listCustomers("Enfield", true);
+//        bank.listCustomers("Southampton", true);
+//        bank.addCustomer("Enfield", "Megs", 30.00);
+//        bank.addCustomer("Southampton", "Megs", 20.00);
     }
 }
