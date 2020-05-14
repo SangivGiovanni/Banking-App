95212e44627cf668d4df27f2296348392a7e806a
Gio K <admin@users-Air.broadband>
2020-05-14 10:09:49 +0100

    Code for running application

---
+++ src/com/giokarunakaran/Branch.java
@@ -0,0 +1,53 @@
+package com.giokarunakaran;
+
+import java.util.ArrayList;
+
+public class Branch {
+    private String name;
+    private ArrayList<Customer> customers;
+
+    public Branch(String name) {
+        this.name = name;
+        this.customers = new ArrayList<Customer>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ArrayList<Customer> getCustomers() {
+        return customers;
+    }
+
+    public boolean newCustomer(String customerName, double initialAmount) {
+        if(findCustomer(customerName) == null) {
+            this.customers.add(new Customer(customerName, initialAmount));
+            return true;
+        } else {
+            System.out.println("Customer Already Exists");
+            return false;
+        }
+    }
+
+    public boolean addCustomerTransaction(String customerName, double amount) {
+        Customer existingCustomer = findCustomer(customerName);
+        if(existingCustomer != null) {
+            existingCustomer.addTransaction(amount);
+            return true;
+        } else {
+            System.out.println("Customer does not Exist");
+            return false;
+        }
+    }
+
+    private Customer findCustomer(String customerName) {
+        for(int i = 0; i < this.customers.size(); i++) {
+            Customer checkedCustomer = this.customers.get(i);
+            if(checkedCustomer.getName().equals(customerName)) {
+                return checkedCustomer;
+            }
+        }
+
+        return null;
+    }
+}
